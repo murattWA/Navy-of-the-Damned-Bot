c28349a9dafc68bd70b6e67cfeb3827569a04d8d
Muhammed Yilmaz <yilmaz@email.arizona.edu>
2023-01-26 01:04:08 -0700

    move players from board to ship as board instances

--- ship.py
+++ ship.py
@@ -7,18 +7,27 @@ class Ship():
     def __init__(self):
         pass
 
+    # TODO add self.mode as a attribute to for game logic to know its playing as an ai
+    
     def start_game(self, member: str):
+        """creates new game against arg: member
+
+        Args:
+            member (str): set to p2 in game if member is == to bot name then p2 will be treated as ai. aka random guesser because its easier :D
+        """
+
         self.member = member.lower()
         if self.member == "navyofthedamnedbot":
             # start game against ai
             pass
         else:
             # start game against member
-            self.brd = Board()
+            self.p1 = Board()
+            self.p2 = Board()
         pass
 
-
-# Board class could be made better and more universal by using a for loop but i feel this is easier to understand at a glance
+    # TODO game logic will go in here as methods attack(), check_winner(), etc.
+    
 class Board():
     """Handles all board logic. dont use 'board' as instance name since thats used in the class. new_board() is called on init."""
 
@@ -28,24 +37,17 @@ class Board():
     def new_board(self):
         """IMPORTANT: when accessing or using x,y coord in self.board remember that its y first then x
         
-        Returns: new boards and ship positions for both players
+        Sets: new board and ship positions
         
         Example:
         for cood (1,2) you would use self.board[2][1]
         """
-        # player 1 board and ships
-        self.boardp1 = [[' ' for x in range(3)] for y in range(3)]
-        self.p1ship1 = self.gen_cord()
-        self.p1ship2 = self.gen_cord(self.p1ship1)
-        self.boardp1[self.p1ship1[1]][self.p1ship1[0]] = "X"
-        self.boardp1[self.p1ship2[1]][self.p1ship2[0]] = "X"
-
-        # player 2 board and ships
-        self.boardp2 = [[' ' for x in range(3)] for y in range(3)]
-        self.p2ship1 = self.gen_cord()
-        self.p2ship2 = self.gen_cord(self.p2ship1)
-        self.boardp2[self.p2ship1[1]][self.p2ship1[0]] = "X"
-        self.boardp2[self.p2ship2[1]][self.p2ship2[0]] = "X"
+        # player board and ships
+        self.board = [[' ' for x in range(3)] for y in range(3)]
+        self.ship1 = self.gen_cord()
+        self.ship2 = self.gen_cord(self.ship1)
+        self.board[self.ship1[1]][self.ship1[0]] = "X"
+        self.board[self.ship2[1]][self.ship2[0]] = "X"
 
     def gen_cord(self, *exclude: list) -> list:
         """generates random list len=2 to be used as x,y coors for board piece placement.
@@ -56,18 +58,9 @@ class Board():
         Returns:
             list: returns a list that is unique compared to *exclude args
         """
+
         l = [random.randint(0, 2), random.randint(0, 2)]
         while l in exclude:
             l = [random.randint(0, 2), random.randint(0, 2)]
         return l
 
-
-ship = Ship()
-ship.start_game("Mo")
-print(ship.member)
-print(ship.brd.boardp1)
-# for row in brd.boardp1:
-#     print(row)
-# print("-------")
-# for row in brd.boardp2:
-#     print(row)
